42fbdaa128578b6b97ac4e762f026adc465937f4
yuan <664995775@qq.com>
2021-07-12 18:22:21 +0800

    update

---
+++ Leetcode.playground/Pages/143_重排链表.xcplaygroundpage/Contents.swift
@@ -0,0 +1,55 @@
+/**
+ https://leetcode-cn.com/problems/reorder-list/
+ 
+ 给定一个单链表 L：L0→L1→…→Ln-1→Ln ，
+ 将其重新排列后变为： L0→Ln→L1→Ln-1→L2→Ln-2→…
+
+ 你不能只是单纯的改变节点内部的值，而是需要实际的进行节点交换。
+
+ 示例 1:
+
+ 给定链表 1->2->3->4, 重新排列为 1->4->2->3.
+ 示例 2:
+
+ 给定链表 1->2->3->4->5, 重新排列为 1->5->2->4->3.
+
+
+ 来源：力扣（LeetCode）
+ 链接：https://leetcode-cn.com/problems/reorder-list
+ 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+
+class Solution {
+    func reorderList(_ head: ListNode?) {
+        if head == nil || head?.next == nil {
+            return
+        }
+        
+        var p1: ListNode? = head
+        var p2: ListNode? = head
+        while p2?.next != nil && p2?.next?.next != nil {
+            p1 = p1?.next
+            p2 = p2?.next?.next
+        }
+        
+        let preMiddle: ListNode? = p1
+        let preCur: ListNode? = p1?.next
+        while preCur?.next != nil {
+            let cur = preCur?.next
+            preCur?.next = cur?.next
+            cur?.next = preMiddle?.next
+            preMiddle?.next = cur
+        }
+        
+        p1 = head
+        p2 = preMiddle?.next
+        while p1 !== preMiddle {
+            preMiddle?.next = p2?.next
+            p2?.next = p1?.next
+            p1?.next = p2
+            p1 = p2?.next
+            p2 = preMiddle?.next
+        }
+    }
+    
+}
---
+++ Leetcode.playground/Pages/295_数据流的中位数.xcplaygroundpage/Contents.swift
@@ -0,0 +1,81 @@
+/**
+ https://leetcode-cn.com/problems/find-median-from-data-stream/
+ 
+ 中位数是有序列表中间的数。如果列表长度是偶数，中位数则是中间两个数的平均值。
+
+ 例如，
+
+ [2,3,4] 的中位数是 3
+
+ [2,3] 的中位数是 (2 + 3) / 2 = 2.5
+
+ 设计一个支持以下两种操作的数据结构：
+
+ void addNum(int num) - 从数据流中添加一个整数到数据结构中。
+ double findMedian() - 返回目前所有元素的中位数。
+ 示例：
+
+ addNum(1)
+ addNum(2)
+ findMedian() -> 1.5
+ addNum(3)
+ findMedian() -> 2
+ 进阶:
+
+ 如果数据流中所有整数都在 0 到 100 范围内，你将如何优化你的算法？
+ 如果数据流中 99% 的整数都在 0 到 100 范围内，你将如何优化你的算法？
+
+
+ 来源：力扣（LeetCode）
+ 链接：https://leetcode-cn.com/problems/find-median-from-data-stream
+ 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+
+class MedianFinder {
+
+    var nums: [Int]
+    
+    init() {
+        self.nums = [Int]()
+    }
+    
+    func addNum(_ num: Int) {
+        if nums.isEmpty {
+            nums.append(num)
+            return
+        }
+        
+        var start = 0, end = nums.count - 1
+        while start <= end {
+            var mid = start + (end - start) / 2
+            if num > nums[mid] {
+                start = mid + 1
+            } else if num < nums[mid] {
+                end = mid - 1
+            } else {
+                start = mid
+                break
+            }
+        }
+        
+        nums.insert(num, at: start)
+    }
+    
+    func findMedian() -> Double {
+        let count = nums.count
+        var median: Double = 0
+        if count & 1 == 0 {
+            median = Double(nums[count / 2] + nums[(count - 1) / 2]) / 2
+        } else {
+            median = Double(nums[count / 2])
+        }
+        return median
+    }
+}
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * let obj = MedianFinder()
+ * obj.addNum(num)
+ * let ret_2: Double = obj.findMedian()
+ */
