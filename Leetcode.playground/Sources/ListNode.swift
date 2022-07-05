8cef8e2a640a97faa632861cbb30d3963efc84da
yuan <664995775@qq.com>
2022-07-06 00:28:21 +0800

    ListNode类优化

--- Leetcode.playground/Sources/ListNode.swift
+++ Leetcode.playground/Sources/ListNode.swift
@@ -11,11 +11,11 @@ public class ListNode {
 /// 遵守`Equatable`协议，方便写测试用例时判等
 extension ListNode: Equatable {
     public static func == (lhs: ListNode, rhs: ListNode) -> Bool {
-        return lhs.val == rhs.val
-//        return lhs.toArray() == rhs.toArray()
+        return lhs.toArray() == rhs.toArray()
     }
 }
 
+/// 链表的个数
 public extension ListNode {
     var count: Int {
         var count = 0
@@ -29,53 +29,32 @@ public extension ListNode {
 }
 
 public extension ListNode {
-    /// 数组转链表,通过一个数组转换成链表
+    /// 数组转链表,通过一个数组生成成链表
     /// - Parameter list: 列表
     /// - Returns: 链表
-    static func arrayToNode(_ list: [Int]) -> ListNode? {
-        if list.isEmpty { return nil }
-        if list.count == 1 { return ListNode(list.first ?? 0, nil) }
-
-        /// 需要先生成最里面的,再生成外面的,所以list传过来,需要逆序一下处理
-        let items = list.reversed().map { $0 }
-        guard let first = items.first, let last = items.last else { return nil }
-        
-        /// 初始化最后一个节点
-        var lastNode: ListNode? = ListNode(first, nil)
-        
-        /// 去掉第0和最后一个，遍历区间是[1,items.count - 1)
-        for i in 1..<(items.count - 1) {
-            lastNode = ListNode(items[i], lastNode)
+    static func arrayToNode<T: Collection>(_ list: T) -> ListNode? where T.Element == Int {
+        var head: ListNode?
+        var p: ListNode?
+        for element in list {
+            if head == nil {
+                head = ListNode(element)
+                p = head
+            } else {
+                p?.next = ListNode(element)
+                p = p?.next
+            }
         }
-        
-        /// 拼接第一个节点, next为lastNode
-        return ListNode(last, lastNode)
+
+        return head
     }
-    
-//    static func makeLinkedList<T: Collection>(from collection: T) -> ListNode?
-//        where T.Element == Int {
-//        var head: ListNode?
-//        var p: ListNode?
-//        for element in collection {
-//            if head == nil {
-//                head = ListNode(element)
-//                p = head
-//            } else {
-//                p!.next = ListNode(element)
-//                p = p!.next
-//            }
-//        }
-//
-//        return head
-//    }
     
     /// 把链表转换成数组
     func toArray() -> [Int] {
         var array = [Int]()
         var node: ListNode? = self
         while node != nil {
-            array.append(node!.val)
-            node = node!.next
+            array.append(node?.val ?? 0)
+            node = node?.next
         }
         return array
     }
