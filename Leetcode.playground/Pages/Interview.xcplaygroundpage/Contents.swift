57a5c741e738ba0249001a46e01dd40e3a0c5884
bruceyuan <664995775@qq.com>
2022-07-10 22:21:47 +0800

    update

--- Leetcode.playground/Pages/Interview.xcplaygroundpage/Contents.swift
+++ Leetcode.playground/Pages/Interview.xcplaygroundpage/Contents.swift
@@ -1,138 +1,25 @@
-/**
- https://leetcode.cn/problems/two-sum/
- 
- 给定一个整数数组 nums 和一个整数目标值 target，请你在该数组中找出 和为目标值 target  的那 两个 整数，并返回它们的数组下标。
-
- 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素在答案里不能重复出现。
-
- 你可以按任意顺序返回答案。
-
-  
-
- 示例 1：
-
- 输入：nums = [2,7,11,15], target = 9
- 输出：[0,1]
- 解释：因为 nums[0] + nums[1] == 9 ，返回 [0, 1] 。
- 示例 2：
-
- 输入：nums = [3,2,4], target = 6
- 输出：[1,2]
- 示例 3：
-
- 输入：nums = [3,3], target = 6
- 输出：[0,1]
-
-
- 来源：力扣（LeetCode）
- 链接：https://leetcode.cn/problems/two-sum
- 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
- */
-
-
-/**
- 思路,哈希表
- 用空间换时间
- first + second = target
- let first: Int = target - nums[i]
- 以nums数组的value作为key,numbs数组的key,作为value,用一个字典存储所有hashMap
- 当能以hashMap[first]取到对应的值时,说明匹配到了first,下标为hashMap[first],第二个数second下标为i
- 那么就只需要返回[hashMap[first], i]就好了
- */
-
 import Foundation
-import Darwin
-import XCTest
-//_001_两数之和.Test.defaultTestSuite.run()
 
-// t = O(N), s = O(N)
-public class _001_两数之和 {
-//class Solution {
-    public init() {}
-    public func runTestSuite() { _001_两数之和.Test.defaultTestSuite.run() }
-
-    public func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
-        var targetOffset = [Int: Int]()
-
-        for (index, num) in nums.enumerated() {
-            let second: Int = target - num
-            if let first = targetOffset[second] {
-                return [first, index]
-            }
-            
-            targetOffset[num] = index
-        }
-        return [Int]()
+do {
+    let s = _02_01_移除重复节点()
+    let node = ListNode.arrayToNode([1, 2, 3, 3, 2, 1])
+    let result = s.removeDuplicateNodes(node)
+    let answer = ListNode.arrayToNode([1, 2, 3])
+    if let result = result {
+        print("\(s) --- \(result)")
     }
+
+    assert(result == answer)
     
-    public func twoSum1(_ nums: [Int], _ target: Int) -> [Int] {
-        var targetOffset = [Int: Int]()
-
-        for num in nums.enumerated() {
-            if let offset = targetOffset[num.element] {
-                print("offset = \(offset)")
-                return [num.offset, offset]
-            }
-            
-            print("element = \(num.element)")
-            targetOffset[target - num.element] = num.offset
-        }
-        return [Int]()
-    }
-}
-
-public extension _001_两数之和 {
-    class Test: XCTestCase {
-        var s = _001_两数之和()
-        
-//        func testExample1() {
-//            let result = s.twoSum([2, 7, 11, 15], 9).sorted()
-//            let answer = [0, 1].sorted()
-//            XCTAssertEqual(result, answer)
-//        }
-//
-//        func testExample2() {
-//            let result = s.twoSum([3,2,4], 6).sorted()
-//            let answer = [1, 2].sorted()
-//            XCTAssertEqual(result, answer)
-//        }
-//
-//        func testExample3() {
-//            let result = s.twoSum([3,3], 6).sorted()
-//            let answer = [0, 1].sorted()
-//            XCTAssertEqual(result, answer)
-//        }
-        
-        func testExample4() {
-            let result = s.twoSum1([2, 7, 11, 15], 9).sorted()
-            let answer = [0, 1].sorted()
-            XCTAssertEqual(result, answer)
-        }
-        
-//        func testExample5() {
-//            let result = s.twoSum1([3,2,4], 6).sorted()
-//            let answer = [1, 2].sorted()
-//            XCTAssertEqual(result, answer)
-//        }
-//
-//        func testExample6() {
-//            let result = s.twoSum1([3,3], 6).sorted()
-//            let answer = [0, 1].sorted()
-//            XCTAssertEqual(result, answer)
-//        }
-    }
+    _02_01_移除重复节点.Test.defaultTestSuite.run()
 }
 
 do {
-    let s = _001_两数之和()
-//    let result = s.twoSum([2, 5, 11, 7], 9)
-//    print("\(s) --- \(result)")
+    let s = _17_06_2出现的次数()
+    let result = s.numberOf2sInRange(25)
+    print("\(s) --- \(result)")
 
-    assert(s.twoSum([2, 7, 11, 15], 9) == [0,1])
-    assert(s.twoSum([3,2,4], 6) == [1,2])
-    assert(s.twoSum([3,3], 6) == [0,1])
-    
-    _001_两数之和.Test.defaultTestSuite.run()
+    assert(s.numberOf2sInRange(25) == 9)
+    assert(s.numberOf2sInRange(2) == 1)
+    assert(s.numberOf2sInRange(1) == 0)
 }
-
-//: [Next](@next)
