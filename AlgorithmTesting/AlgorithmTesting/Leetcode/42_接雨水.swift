a521140a6284cba8ac88dbbb93b6c67a92f3b100
bruceyuan <664995775@qq.com>
2022-11-13 13:14:54 +0800

    42_接雨水

--- AlgorithmTesting/AlgorithmTesting/Leetcode/42_接雨水.swift
+++ AlgorithmTesting/AlgorithmTesting/Leetcode/42_接雨水.swift
@@ -118,18 +118,18 @@ extension _42_接雨水 {
     }
 }
 
-do {
-    let s = _42_接雨水()
-    let result1 = s.trap([0,1,0,2,1,0,1,3,2,1,2,1])
-    assert(result1 == 6)
-    
-    let result2 = s.trap([4,2,0,3,2,5])
-    assert(result2 == 9)
-
-    print("\(s) result1 = \(result1) ---- result2 = \(result2)")
-
-    /// 对数器测试
-    _42_接雨水.test()
-}
+//do {
+//    let s = _42_接雨水()
+//    let result1 = s.trap([0,1,0,2,1,0,1,3,2,1,2,1])
+//    assert(result1 == 6)
+//
+//    let result2 = s.trap([4,2,0,3,2,5])
+//    assert(result2 == 9)
+//
+//    print("\(s) result1 = \(result1) ---- result2 = \(result2)")
+//
+//    /// 对数器测试
+//    _42_接雨水.test()
+//}
 
 //: [Next](@next)
